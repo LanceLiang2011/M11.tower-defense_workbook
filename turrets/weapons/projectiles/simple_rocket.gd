418c79a26a5d4675bc60c63ccf63f401d269b0c4
LanceLiang2011 <liangxianyi2011@gmail.com>
2025-08-05 23:29:14 -0400

    Refactor SimpleRocket and Weapon classes to enhance functionality and structure

--- turrets/weapons/projectiles/simple_rocket.gd
+++ turrets/weapons/projectiles/simple_rocket.gd
@@ -1,1 +1,27 @@
-extends Area2D
+class_name SimpleRocket extends Area2D
+
+@export var speed: float = 350.0
+@export var max_distance: float = 1000.0
+
+var _traveled_distance: float = 0.0
+
+func _init():
+	monitorable = false # the rocket is not detectable, it detects others
+
+
+func _physics_process(delta: float) -> void:
+	var distance_to_move: float = speed * delta
+	_traveled_distance += distance_to_move
+
+	position += transform.x.normalized() * distance_to_move
+
+	check_distance()
+
+
+func check_distance() -> void:
+	if _traveled_distance >= max_distance:
+		explode()
+
+
+func explode() -> void:
+	queue_free() # Remove the rocket from the scene for now TODO: add explosion effect
--- turrets/weapons/weapon.gd
+++ turrets/weapons/weapon.gd
@@ -3,12 +3,27 @@ class_name Weapon extends Sprite2D
 
 @export var mob_detection_range := 400.0
 
-var _area_2d := _create_area_2d()
+## Number of attacks per second
+@export var attack_rate := 1.0
+
+var _area_2d := _create_area_2d() # No need to onready as there's not dynamic initialization in _create_area_2d()
 @onready var _collision_shape_2d := _create_collision_shape_2d()
+@onready var _timer := _create_timer()
 
 func _ready() -> void:
+	_create_nodes()
+	_connect_signals()
+
+
+func _create_nodes() -> void:
 	add_child(_area_2d)
 	_area_2d.add_child(_collision_shape_2d)
+	add_child(_timer)
+
+
+func _connect_signals() -> void:
+	_timer.timeout.connect(_attack)
+
 
 func _create_area_2d() -> Area2D:
 	var area := Area2D.new()
@@ -28,3 +43,16 @@ func _create_collision_shape_2d() -> CollisionShape2D:
 	shape.shape = circle_shape
 
 	return shape
+
+
+func _create_timer() -> Timer:
+	var timer := Timer.new()
+
+	var wait_time := 1.0 / attack_rate
+	timer.wait_time = wait_time
+
+	return timer
+
+
+func _attack() -> void:
+	print("Attacking!")
