f023d1645710e69631c70e9b67ac9576f8b861c5
LanceLiang2011 <liangxianyi2011@gmail.com>
2025-08-07 23:59:22 -0400

    Refactor MobPathFollow class to extend PathFollow2D; implement mob assignment and movement logic

--- mobs/mob_path_follow.gd
+++ mobs/mob_path_follow.gd
@@ -1,1 +1,18 @@
-class_name MobPathFollow extends Node
+class_name MobPathFollow extends PathFollow2D
+
+var mob: Mob = null: set = set_mob
+
+
+func _ready() -> void:
+	if mob == null and get_child_count() > 0:
+		mob = get_child(0) as Mob # We assume the mob should be the direct child of the path
+
+
+func _physics_process(delta: float) -> void:
+	progress += mob.speed * delta
+
+func set_mob(new_mob: Mob) -> void:
+	mob = new_mob
+
+	if mob != null:
+		mob.tree_exited.connect(queue_free) # When mob is removed, it's path will be removed too. In this design, each mob should have one path follow node.
